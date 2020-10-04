e04c13ca19e915e499f63e117f2f2b5446ebc177
yamamotoshota0903 <nihikinoani18@outlook.jp>
2020-10-04 19:05:20 +0900

    Updates

---
+++ precache-manifest.88d6ffb53e210932d682eb9bc549fd5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8cd69132c3cc65d0e42576f7d203ce6",
+    "url": "/smart-hospital-patientUI/index.html"
+  },
+  {
+    "revision": "f20ab78bfca06d7ded88",
+    "url": "/smart-hospital-patientUI/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb1582bc14ad1efec1ec",
+    "url": "/smart-hospital-patientUI/static/js/2.fdb55326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-hospital-patientUI/static/js/2.fdb55326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20ab78bfca06d7ded88",
+    "url": "/smart-hospital-patientUI/static/js/main.f6844549.chunk.js"
+  },
+  {
+    "revision": "d538c13a50007b13c8a4",
+    "url": "/smart-hospital-patientUI/static/js/runtime-main.87f42ed8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smart-hospital-patientUI/static/media/logo.5d5d9eef.svg"
+  }
+]);
